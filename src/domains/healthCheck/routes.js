f8c720c0d4d9febf0a1c9fb5866c01e93808be08
ramos <rdiego26@gmail.com>
2018-05-30 02:16:35 -0300

    USE schema .

--- src/domains/healthCheck/routes.js
+++ src/domains/healthCheck/routes.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const middleware = require('./middlewares');
+const schemas = require('./schemas');
 
 const routes = [
 	{ method: 'get',
 		path: '/healthCheck',
-		handler: middleware.simple
+		handler: middleware.simple,
+		schema: schemas.simple,
 	}
 ];
 
---
+++ src/domains/healthCheck/schemas.js
@@ -0,0 +1,12 @@
+module.exports = {
+	simple: {
+		response: {
+			200: {
+				type: 'object',
+				properties: {
+					result: { type: 'string' }
+				}
+			}
+		}
+	}
+};
